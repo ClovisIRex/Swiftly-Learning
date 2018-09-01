3424faad0625a41e3493ea05c4d817db3c18959b
Tal Livny <livnytal.dev@gmail.com>
2018-09-01 23:32:59 +0300

    init

---
+++ first/first/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  first
+//
+//  Created by Tal Livny on 01/09/2018.
+//  Copyright © 2018 Tal Livny. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
