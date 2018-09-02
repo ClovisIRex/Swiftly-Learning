51bb3f961fe6e0953be272369cd602277f8568f4
Tal Livny <livnytal.dev@gmail.com>
2018-09-03 00:45:36 +0300

    The programming q=equivalent of Portuguese

--- first/first/main.swift
+++ first/first/main.swift
@@ -7,6 +7,29 @@
 //
 
 import Foundation
+//
+//let mylet = "asd"
+//
+//var name2: Double = 2.5
+//
+//
+//print(mylet, name2)
 
-print("Hello, World!")
 
+
+//var optionalString: String? = "Hello"
+//print(optionalString == nil)
+//
+//var optionalName: String? = "afas"
+//var greeting = "Hello!"
+//if let name = optionalName {
+//    greeting = "Hello, \(name)"
+//}
+//
+//print(greeting)
+
+
+func greet(_ person: String, on day: String) -> String {
+    return "Hello \(person), today is \(day)."
+}
+print(greet("John", on: "Wednesday"))
